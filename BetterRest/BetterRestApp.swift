1129d2c6ba727794608de9c02cee441a3fafdd21
Johnny Martinez <johnnymn@hotmail.com>
2021-10-07 09:28:28 -0700

    Format swift files using swiftformat

--- BetterRest/BetterRestApp.swift
+++ BetterRest/BetterRestApp.swift
@@ -1,29 +1,31 @@
 import SwiftUI
 
 class AppDelegate: NSObject, UIApplicationDelegate {
-  func application(_ application: UIApplication,
-                   didFinishLaunchingWithOptions launchOptions: [
-                   UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-    #if DEBUG
-    var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
-    #if targetEnvironment(macCatalyst)
-    injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
-    #elseif os(iOS)
-    injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
-    #endif
-    Bundle(path: injectionBundlePath)?.load()
-    #endif
-    return true
-  }
+    func application(_: UIApplication,
+                     didFinishLaunchingWithOptions _: [
+                         UIApplication.LaunchOptionsKey: Any
+                     ]?) -> Bool
+    {
+        #if DEBUG
+            var injectionBundlePath = "/Applications/InjectionIII.app/Contents/Resources"
+            #if targetEnvironment(macCatalyst)
+                injectionBundlePath = "\(injectionBundlePath)/macOSInjection.bundle"
+            #elseif os(iOS)
+                injectionBundlePath = "\(injectionBundlePath)/iOSInjection.bundle"
+            #endif
+            Bundle(path: injectionBundlePath)?.load()
+        #endif
+        return true
+    }
 }
 
 @main
 struct BetterRestApp: App {
-  @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
 
-  var body: some Scene {
-    WindowGroup {
-      ContentView()
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
     }
-  }
 }
--- BetterRest/ContentView.swift
+++ BetterRest/ContentView.swift
@@ -2,118 +2,121 @@ import CoreML
 import SwiftUI
 
 struct ContentView: View {
-  // How much sleep the user wants.
-  @State private var sleepAmount: Double = 8
-  // How much coffee they drink.
-  @State private var coffeeAmount = 1
-  // When the user wants to wake up.
-  @State private var wakeup = defaultWakeTime
+    // How much sleep the user wants.
+    @State private var sleepAmount: Double = 8
+    // How much coffee they drink.
+    @State private var coffeeAmount = 1
+    // When the user wants to wake up.
+    @State private var wakeup = defaultWakeTime
 
-  static var defaultWakeTime: Date {
-    var components = DateComponents()
-    components.hour = 7
-    components.minute = 0
+    static var defaultWakeTime: Date {
+        var components = DateComponents()
+        components.hour = 7
+        components.minute = 0
 
-    return Calendar.current.date(from: components) ?? Date()
-  }
-
-  // CoreML Tabular Regressor model.
-  let model: SleepCalculator = {
-    do {
-      return try SleepCalculator(configuration: MLModelConfiguration())
-    } catch {
-      print(error)
-      fatalError("Couldn't create SleepCalculator")
-    }
-  }()
-
-  // Estimates the time that the user should go to bed
-  // to get the desired amount of sleep considering the
-  // amount of coffee consumed, and displays it on a
-  // text box.
-  // The prediction is done by the CoreML model.
-  private var bedTime: String {
-    // Break down the wakeup date into hour/minutes.
-    let components = Calendar.current.dateComponents(
-            [.hour, .minute], from: wakeup)
-    let hour = (components.hour ?? 0) * 60 * 60
-    let minutes = (components.minute ?? 0) * 60
-    var bedTime = ""
-
-    // The CoreML prediction can fail.
-    do {
-      let prediction = try model.prediction(
-              wake: Double(hour + minutes),
-              estimatedSleep: sleepAmount,
-              coffee: Double(coffeeAmount))
-
-      let sleepTime = wakeup - prediction.actualSleep
-      let formatter = DateFormatter()
-      formatter.timeStyle = .short
-      bedTime = formatter.string(from: sleepTime)
-    } catch {
-      // Something went wrong, show an alert
-      alertTitle = "Error"
-      alertMessage = "Sorry, there was a problem calculating your bedtime"
-      showingAlert = true
+        return Calendar.current.date(from: components) ?? Date()
     }
 
-    return bedTime
-  }
+    // CoreML Tabular Regressor model.
+    let model: SleepCalculator = {
+        do {
+            return try SleepCalculator(configuration: MLModelConfiguration())
+        } catch {
+            print(error)
+            fatalError("Couldn't create SleepCalculator")
+        }
+    }()
 
-  // Prediction output alert.
-  @State private var alertTitle = ""
-  @State private var alertMessage = ""
-  @State private var showingAlert = false
+    // Estimates the time that the user should go to bed
+    // to get the desired amount of sleep considering the
+    // amount of coffee consumed, and displays it on a
+    // text box.
+    // The prediction is done by the CoreML model.
+    private var bedTime: String {
+        // Break down the wakeup date into hour/minutes.
+        let components = Calendar.current.dateComponents(
+            [.hour, .minute], from: wakeup
+        )
+        let hour = (components.hour ?? 0) * 60 * 60
+        let minutes = (components.minute ?? 0) * 60
+        var bedTime = ""
 
-  var body: some View {
-    NavigationView {
-      Form {
-        // Put each pair of text view and control in a
-        // VStack so each is displayed as a single row
-        // in the form
-        Section(header: Text("When do you want to wake up?")) {
-          DatePicker("Please enter a time",
-                  selection: $wakeup,
-                  displayedComponents: .hourAndMinute)
-                  .labelsHidden()
-                  .datePickerStyle(WheelDatePickerStyle())
+        // The CoreML prediction can fail.
+        do {
+            let prediction = try model.prediction(
+                wake: Double(hour + minutes),
+                estimatedSleep: sleepAmount,
+                coffee: Double(coffeeAmount)
+            )
+
+            let sleepTime = wakeup - prediction.actualSleep
+            let formatter = DateFormatter()
+            formatter.timeStyle = .short
+            bedTime = formatter.string(from: sleepTime)
+        } catch {
+            // Something went wrong, show an alert
+            alertTitle = "Error"
+            alertMessage = "Sorry, there was a problem calculating your bedtime"
+            showingAlert = true
         }
 
-        Section(header: Text("Desired amount of sleep")) {
-          Stepper(value: $sleepAmount, in: 4...12, step: 0.25) {
-            Text("\(sleepAmount, specifier: "%g")")
-          }
-        }
-
-        Section(header: Text("Daily coffee intake")) {
-          Stepper(value: $coffeeAmount, in: 1...20) {
-            Text(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups")
-          }
-        }
-
-        Section(header: Text("Recommended bed time")) {
-          Text("\(bedTime)")
-        }
-      }.navigationBarTitle("Better Rest").alert(isPresented: $showingAlert) {
-        Alert(
-                title: Text(alertTitle),
-                message: Text(alertMessage),
-                dismissButton: .default(Text("Ok")))
-      }
+        return bedTime
     }
-  }
+
+    // Prediction output alert.
+    @State private var alertTitle = ""
+    @State private var alertMessage = ""
+    @State private var showingAlert = false
+
+    var body: some View {
+        NavigationView {
+            Form {
+                // Put each pair of text view and control in a
+                // VStack so each is displayed as a single row
+                // in the form
+                Section(header: Text("When do you want to wake up?")) {
+                    DatePicker("Please enter a time",
+                               selection: $wakeup,
+                               displayedComponents: .hourAndMinute)
+                        .labelsHidden()
+                        .datePickerStyle(WheelDatePickerStyle())
+                }
+
+                Section(header: Text("Desired amount of sleep")) {
+                    Stepper(value: $sleepAmount, in: 4 ... 12, step: 0.25) {
+                        Text("\(sleepAmount, specifier: "%g")")
+                    }
+                }
+
+                Section(header: Text("Daily coffee intake")) {
+                    Stepper(value: $coffeeAmount, in: 1 ... 20) {
+                        Text(coffeeAmount == 1 ? "1 cup" : "\(coffeeAmount) cups")
+                    }
+                }
+
+                Section(header: Text("Recommended bed time")) {
+                    Text("\(bedTime)")
+                }
+            }.navigationBarTitle("Better Rest").alert(isPresented: $showingAlert) {
+                Alert(
+                    title: Text(alertTitle),
+                    message: Text(alertMessage),
+                    dismissButton: .default(Text("Ok"))
+                )
+            }
+        }
+    }
 }
 
 class ContentView_Previews: PreviewProvider {
-  static var previews: some View {
-    ContentView()
-  }
+    static var previews: some View {
+        ContentView()
+    }
 
-  #if DEBUG
-  @objc class func injected() {
-    UIApplication.shared.windows.first?.rootViewController =
-            UIHostingController(rootView: ContentView())
-  }
-  #endif
+    #if DEBUG
+        @objc class func injected() {
+            UIApplication.shared.windows.first?.rootViewController =
+                UIHostingController(rootView: ContentView())
+        }
+    #endif
 }
